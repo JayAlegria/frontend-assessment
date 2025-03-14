4772bb9747284f27738f80a1a4847a41d46248ea
Jay <jay.wbrides@gmail.com>
2025-03-14 13:27:21 +0800

    Feat: redirect app homepage to dashboard/card

--- project/next.config.js
+++ project/next.config.js
@@ -3,6 +3,24 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard/card", 
+      },
+    ]
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/dashboard/card",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
